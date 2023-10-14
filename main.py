fixed underflow error, model correctly classifies

main.py
@@ -1,16 +1,18 @@
 # UI file
 
-
+from simplegmail import Gmail
+from gaussian import get_score
+from email_class import Email
+from simplegmail.query import construct_query
 import customtkinter as ctk
 from tkinter.constants import*
-import numpy as np
-
-# Loading the data
-file = open("data\\training_data.npy", "rb")
-emails_array = np.load(file, allow_pickle=True)
 
 
+#initialization
+gmail = Gmail()
 
+
+# UI
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
 
@@ -19,26 +21,62 @@ root.geometry("800x500")
 root.title("scribo")
 root.resizable(False, False)
 
-# class EmailUI:
+currently_open = []
 
-#     def __init__(self, email_object):
+class EmailUI:
+    summary = ""
 
 
+    def get_summary():
+        pass
 
-def show_email():
+    def __init__(self, master, email_text, email_obj, row_index, importance):
+        master.label = ctk.CTkButton(master=master, text= email_text, command= lambda: display_email(email_obj), width=550, height=20)
+        master.label.grid(row=row_index, column=0, padx=0)
+
+def display_email(email):
+    
+    n = ctk.CTkLabel(master=frame, text=email.plain, font=('Verdana', 7))
+    print(email.plain)
+    n.pack()
+    for x in currently_open:
+        x.destroy()
+
+
+def classify_email(email):
+    pass
+
+def show_emails(msgs):
     print("opened email")
     
     email_scroll = ctk.CTkScrollableFrame(master=frame, width=550, height=400)
     email_scroll.pack()
     email_scroll.place(relx=0.27, rely=0.1)
+    currently_open.append(email_scroll)
 
     count = 0
-    for i in range(0, 500, 20):
-        t = emails_array[count].subject
-        email_scroll.label = ctk.CTkLabel(master=email_scroll, text=t)
-        email_scroll.label.grid(row=i, column=0, padx=20)
-        if count <= len(emails_array) - 2:
+    for i in range(0, 20 * len(msgs), 20):
+        t = get_score(Email(msgs[count]))
+        text = msgs[count].subject
+        if len(text) > 65:
+            t += text[0:65] + "..."
+        else:
+            t += text
+        EmailUI(email_scroll, t, msgs[count], i, 1)
+        if count <= len(msgs) - 2:
             count += 1
+
+
+
+
+# grabbing recent unread emails to display
+query_params = {
+    "newer_than": (14, "day"),
+    "unread": True,
+}
+messages = gmail.get_messages(query=construct_query(query_params))
+
+
 
 
 
@@ -49,9 +87,10 @@ label = ctk.CTkLabel(master=frame, text="Demo version", font=('Verdana', 20))
 label.pack(pady=12, padx=10)
 label.place(relx=0.05, rely=0.1)
 
-button = ctk.CTkButton(master=frame, text="Open", command=show_email)
+button = ctk.CTkButton(master=frame, text="Open", command= lambda: show_emails(messages))
 button.pack(pady=12, padx=10)
 button.place(relx=0.05, rely=0.2)
+
 
 
 

process_data.py
@@ -2,6 +2,24 @@
 # in the classification algorithm.
 
 import numpy as np
+def process_list(main_list, properties_list):
+        main_list[0][0] = np.mean(properties_list[0])
+        main_list[1][0] = np.std(properties_list[0])
+
+        main_list[0][1] = np.mean(properties_list[1])
+        main_list[1][1] = np.std(properties_list[1])
+
+        main_list[0][2] = np.mean(properties_list[2])
+        main_list[1][2] = np.std(properties_list[2])
+
+        main_list[0][3] = np.mean(properties_list[3])
+        main_list[1][3] = np.std(properties_list[3])
+
+        main_list[0][4] = np.mean(properties_list[4])
+        main_list[1][4] = np.std(properties_list[4])
+
+        main_list[0][5] = np.mean(properties_list[5])
+        main_list[1][5] = np.std(properties_list[5])
 
 def process_training_data():
 
@@ -24,24 +42,7 @@ def process_training_data():
     i_all_lists = [i_sender_email_length_list, i_time_sent_list, i_attachments_list, i_subject_length_list, i_length_list, i_num_verbs_list]
 
 
-    def process_list(main_list, properties_list):
-        main_list[0][0] = np.mean(properties_list[0])
-        main_list[1][0] = np.std(properties_list[0])
-
-        main_list[0][1] = np.mean(properties_list[1])
-        main_list[1][1] = np.std(properties_list[1])
-
-        main_list[0][2] = np.mean(properties_list[2])
-        main_list[1][2] = np.std(properties_list[2])
-
-        main_list[0][3] = np.mean(properties_list[3])
-        main_list[1][3] = np.std(properties_list[3])
-
-        main_list[0][4] = np.mean(properties_list[4])
-        main_list[1][4] = np.std(properties_list[4])
-
-        main_list[0][5] = np.mean(properties_list[5])
-        main_list[1][5] = np.std(properties_list[5])
+    
 
 
 
@@ -95,18 +96,29 @@ def process_training_data():
     important_properties = [[0 for i in range(6)] for x in range(2)] # 2 x 6 list
     process_list(important_properties, i_all_lists)
 
+    # writing array to file
+    file2 = open("data\\important_data_array.npy", "wb")
+    np.save(file2, important_properties)
+    file2.close()
+
+     # writing array to file
+    file3 = open("data\\unimportant_data_array.npy", "wb")
+    np.save(file3, unimportant_properties)
+    file3.close()
 
 
-    # # test prints
-    # print("unimportant")
-    # for row in range (0, 2):
-    #     for column in range (0, 6):
-    #         print(unimportant_properties[row][column], end=" ")
-    #     print(" ")
 
-    # print("important")
-    # for row in range (0, 2):
-    #     for column in range (0, 6):
-    #         print(important_properties[row][column], end=" ")
-    #     print(" ")
+    # test prints
+    print("unimportant")
+    for row in range (0, 2):
+        for column in range (0, 6):
+            print(unimportant_properties[row][column], end=" ")
+        print(" ")
 
+    print("important")
+    for row in range (0, 2):
+        for column in range (0, 6):
+            print(important_properties[row][column], end=" ")
+        print(" ")
+
+process_training_data()